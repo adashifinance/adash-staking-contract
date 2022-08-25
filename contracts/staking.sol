777c7bcf48cbec61cb00e3168ecb5bb73a13fb6e
blackalbino01 <hauwal4969@gmail.com>
2022-08-25 19:02:20 +0100

    update

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -1,11 +1,19 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Staking is ReentrancyGuard {
+contract Staking is ReentrancyGuard, Ownable {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    //staking token address
+    IERC20 public token;
+
     // Staker info
     struct Staker {
         // The deposited tokens of the Staker
@@ -17,17 +25,22 @@ contract Staking is ReentrancyGuard {
         uint256 unclaimedRewards;
     }
 
-    // Rewards per hour. A fraction calculated as x/10.000.000 to get the percentage
-    uint256 public rewardsPerHour = 285; // 0.00285%/h or 25% APR
+    // Rewards per year. A fraction calculated as x/10000 to get the percentage
+    uint256 public rewardsPerYear;
 
     // Minimum amount to stake
-    uint256 public minStake = 10 * 10**decimals();
+    uint256 constant minStake = 10 ether;
 
     // Compounding frequency limit in seconds
-    uint256 public compoundFreq = 14400; //4 hours
+    uint256 constant compoundFreq = 31536000; // 86400 hours 365 days
 
     // Mapping of address to Staker info
     mapping(address => Staker) internal stakers;
+
+    constructor(address _token, uint256 _rewardsPerYear){
+        token = IERC20(_token);
+        rewardsPerYear = _rewardsPerYear;
+    }
 
 
     // If address has no Staker struct, initiate one. If address already was a stake,
@@ -37,7 +50,7 @@ contract Staking is ReentrancyGuard {
     function deposit(uint256 _amount) external nonReentrant {
         require(_amount >= minStake, "Amount smaller than minimimum deposit");
         require(
-            balanceOf(msg.sender) >= _amount,
+            token.balanceOf(msg.sender) >= _amount,
             "Can't stake more than you own"
         );
         if (stakers[msg.sender].deposited == 0) {
@@ -46,11 +59,11 @@ contract Staking is ReentrancyGuard {
             stakers[msg.sender].unclaimedRewards = 0;
         } else {
             uint256 rewards = calculateRewards(msg.sender);
-            stakers[msg.sender].unclaimedRewards += rewards;
-            stakers[msg.sender].deposited += _amount;
+            stakers[msg.sender].unclaimedRewards.add(rewards);
+            stakers[msg.sender].deposited.add(_amount);
             stakers[msg.sender].timeOfLastUpdate = block.timestamp;
         }
-        _burn(msg.sender, _amount);
+        token.safeTransferFrom(msg.sender, address(this), _amount);
     }
 
     // Compound the rewards and reset the last time of update for Deposit info
@@ -58,23 +71,21 @@ contract Staking is ReentrancyGuard {
         require(stakers[msg.sender].deposited > 0, "You have no deposit");
         require(
             compoundRewardsTimer(msg.sender) == 0,
-            "Tried to compound rewars too soon"
+            "Tried to compound rewards too soon"
         );
-        uint256 rewards = calculateRewards(msg.sender) +
-            stakers[msg.sender].unclaimedRewards;
+        uint256 rewards = calculateRewards(msg.sender).add(stakers[msg.sender].unclaimedRewards);
         stakers[msg.sender].unclaimedRewards = 0;
-        stakers[msg.sender].deposited += rewards;
+        stakers[msg.sender].deposited.add(rewards);
         stakers[msg.sender].timeOfLastUpdate = block.timestamp;
     }
 
-    // Mints rewards for msg.sender
+    // transfer rewards for msg.sender
     function claimRewards() external nonReentrant {
-        uint256 rewards = calculateRewards(msg.sender) +
-            stakers[msg.sender].unclaimedRewards;
+        uint256 rewards = calculateRewards(msg.sender).add(stakers[msg.sender].unclaimedRewards);
         require(rewards > 0, "You have no rewards");
         stakers[msg.sender].unclaimedRewards = 0;
         stakers[msg.sender].timeOfLastUpdate = block.timestamp;
-        _mint(msg.sender, rewards);
+        token.safeTransfer(msg.sender, rewards);
     }
 
     // Withdraw specified amount of staked tokens
@@ -84,22 +95,21 @@ contract Staking is ReentrancyGuard {
             "Can't withdraw more than you have"
         );
         uint256 _rewards = calculateRewards(msg.sender);
-        stakers[msg.sender].deposited -= _amount;
+        stakers[msg.sender].deposited.sub(_amount);
         stakers[msg.sender].timeOfLastUpdate = block.timestamp;
         stakers[msg.sender].unclaimedRewards = _rewards;
-        _mint(msg.sender, _amount);
+        token.safeTransfer(msg.sender, _amount);
     }
 
-    // Withdraw all stake and rewards and mints them to the msg.sender
+    // Withdraw all stake and rewards the msg.sender
     function withdrawAll() external nonReentrant {
         require(stakers[msg.sender].deposited > 0, "You have no deposit");
-        uint256 _rewards = calculateRewards(msg.sender) +
-            stakers[msg.sender].unclaimedRewards;
+        uint256 _rewards = calculateRewards(msg.sender).add(stakers[msg.sender].unclaimedRewards);
         uint256 _deposit = stakers[msg.sender].deposited;
         stakers[msg.sender].deposited = 0;
         stakers[msg.sender].timeOfLastUpdate = 0;
         uint256 _amount = _rewards + _deposit;
-        _mint(msg.sender, _amount);
+        token.safeTransfer(msg.sender, _amount);
     }
 
     // Function useful for fron-end that returns user stake and rewards by address
@@ -110,8 +120,7 @@ contract Staking is ReentrancyGuard {
     {
         _stake = stakers[_user].deposited;
         _rewards =
-            calculateRewards(_user) +
-            stakers[msg.sender].unclaimedRewards;
+            calculateRewards(_user).add(stakers[msg.sender].unclaimedRewards);
         return (_stake, _rewards);
     }
 
@@ -125,8 +134,7 @@ contract Staking is ReentrancyGuard {
             return 0;
         } else {
             return
-                (stakers[_user].timeOfLastUpdate + compoundFreq) -
-                block.timestamp;
+                (stakers[_user].timeOfLastUpdate.add(compoundFreq)).sub(block.timestamp);
         }
     }
 
@@ -136,7 +144,10 @@ contract Staking is ReentrancyGuard {
         view
         returns (uint256 rewards)
     {
-        return (((((block.timestamp - stakers[_staker].timeOfLastUpdate) *
-            stakers[_staker].deposited) * rewardsPerHour) / 3600) / 10000000);
+        return (((block.timestamp.sub(stakers[_staker].timeOfLastUpdate)).mul(stakers[_staker].deposited).mul(rewardsPerYear)) / 10000);
+    }
+
+    function setRewardDuration(uint256 _rewardDuration) external onlyOwner{
+        rewardsPerYear = _rewardDuration;
     }
 }
